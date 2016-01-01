743276ed5bd7dccbc0c0530835185af4d0f6a851
vikramgorla <vikram.gorla@gmail.com>
2016-01-01 22:21:25 +0100

    Update nfs-client.rb

--- attributes/nfs-client.rb
+++ attributes/nfs-client.rb
@@ -1,7 +1,7 @@
 # clients
-default['magentostack']['nfs_client']['mount_point'] = '/mnt/magento_media'
-default['magentostack']['nfs_client']['symlink_target'] = 'media'
+node.default['magentostack']['nfs_client']['mount_point'] = '/mnt/magento_media'
+node.default['magentostack']['nfs_client']['symlink_target'] = 'media'
 
-default['magentostack']['nfs_client']['nfs_server_node'] = false
-default['magentostack']['nfs_client']['export_name'] = false
-default['magentostack']['nfs_client']['export_root'] = false
+node.default['magentostack']['nfs_client']['nfs_server_node'] = false
+node.default['magentostack']['nfs_client']['export_name'] = false
+node.default['magentostack']['nfs_client']['export_root'] = false
