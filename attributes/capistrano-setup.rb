b0cc87be7fe4bf746c69ead41e3b1e561036760a
vikramgorla <vikram.gorla@gmail.com>
2016-01-01 21:42:00 +0100

    Create capistrano-setup.rb

---
+++ attributes/capistrano-setup.rb
@@ -0,0 +1,6 @@
+node.default['magento']['capistrano']['docroot'] = '/var/www/html/magento'
+node.default['magento']['capistrano']['folder_owner'] = 'apache'
+node.default['magento']['capistrano']["app_shared_dirs"] = ["/app/etc", "/sitemaps", "/media", "/var", "/staging"]
+node.default['magento']['capistrano']["app_shared_files"] = ["/app/etc/local.xml"]
+node.default['magento']['capistrano']["nfs_path"] = false
+node.default['magento']['capistrano']["nfs_symlinks"] = ["/media", "/staging", "/sitemaps", "/var/locks"]
