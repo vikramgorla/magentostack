50c1404a8781b387de8b598cb32ae5192d3c04ac
vikramgorla <vikram.gorla@gmail.com>
2016-01-02 18:20:27 +0100

    Update varnish.rb

--- attributes/varnish.rb
+++ attributes/varnish.rb
@@ -1,23 +1,19 @@
-# Encoding: utf-8
-#
-# Cookbook Name:: magentostack
-# Attributes:: varnish
-#
-# Copyright 2014, Rackspace Hosting
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-default['magentostack']['varnish']['listen_port'] = 8080
-default['magentostack']['varnish']['secret'] = false
-default['magentostack']['varnish']['config'] = true
+default['magento']['varnish']['backend_servers'] = [
+    {
+        "name" => "web1",
+        "ip" => "127.0.0.1",
+        "connect_timeout" => '240s',
+        "first_byte_timeout" => '240s',
+        "between_bytes_timeout" => '240s',
+        "max_connections" => 800
+    }
+]
+default['magento']['varnish']['trusted_servers'] = [
+    "127.0.0.1"
+]
+default['magento']['varnish']['ttl_for_static_files'] = '30d'
+default['magento']['varnish']['probe']['timeout'] = '90s'
+default['magento']['varnish']['additional_vcls'] = []
+default['magento']['varnish']['additional_recv_subs'] = []
+default['magento']['varnish']['additional_hash_subs'] = []
+default['magento']['varnish']['director_strategy'] = 'random'
