204abdd65c68c799d1d096f64b305d86263600f9
vikramgorla <vikram.gorla@gmail.com>
2016-01-07 17:13:28 +0100

    Update apache-htaccess.rb

--- attributes/apache-htaccess.rb
+++ attributes/apache-htaccess.rb
@@ -1,5 +1,2 @@
 node.default['apache']['htpasswd_user'] = 'strongusername'
 node.default['apache']['htpasswd_password'] = 'strongpassword'
-
-#default value for mod_extract_forwarded (if used) to be overrided if you need 
-node['apache']['mod_extract_forwarded']['accept'] = ['all']
