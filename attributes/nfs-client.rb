4564de2eda7df5c62f2dad0dfc12b0a7221b78d0
vikramgorla <vikram.gorla@gmail.com>
2016-01-13 13:11:38 +0100

    Update nfs-client.rb

--- attributes/nfs-client.rb
+++ attributes/nfs-client.rb
@@ -5,3 +5,4 @@ node.default['magentostack']['nfs_client']['symlink_target'] = 'media'
 node.default['magentostack']['nfs_client']['nfs_server_node'] = false
 node.default['magentostack']['nfs_client']['export_name'] = false
 node.default['magentostack']['nfs_client']['export_root'] = false
+default['magentostack']['nfs_client']['enable_mount'] = true
